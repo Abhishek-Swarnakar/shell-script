3f7a42ad208970a0d1151c74809ab33bde4cf4de
Abhishek-Swarnakar <abhishekswarnakar2000iem@gmail.com>
2024-11-12 13:15:23 +0530

    uploading

--- script5/script.sh
+++ script5/script.sh
@@ -5,7 +5,11 @@ DIRECTORY="/home/ubuntu/folder"
 if [ ! -d "$DIRECTORY" ]; then
     echo "$DIRECTORY does not exist"
     exit 1
-else    
-    find "$DIRECTORY" -type f -mtime +30 -exec rm {} \;
+fi
+OLDFILES=$(find "$DIRECTORY" -type -f -mtime +30)  
+if [ -n "$OLDFILES" ]; then
+    echo "$OLDFILES" | xargs rm
     echo "DELETED FILES OLDER THAN 30 DAYS INSIDE DIRECTORY: $DIRECTORY"
+else
+    "NO FILES GREATER THAN 30 DAYS!!"
 fi
