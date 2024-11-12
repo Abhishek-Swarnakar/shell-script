216a86e264c87824363d7e14ef0a14971afab3e6
Abhishek-Swarnakar <abhishekswarnakar2000iem@gmail.com>
2024-11-12 13:19:02 +0530

    uploading

--- script5/script.sh
+++ script5/script.sh
@@ -6,7 +6,7 @@ if [ ! -d "$DIRECTORY" ]; then
     echo "$DIRECTORY does not exist"
     exit 1
 fi
-OLDFILES=$(find "$DIRECTORY" -type -f -mtime +30)  
+OLDFILES=$(find "$DIRECTORY" -type f -mtime +30)  
 if [ -n "$OLDFILES" ]; then
     echo "$OLDFILES" | xargs rm
     echo "DELETED FILES OLDER THAN 30 DAYS INSIDE DIRECTORY: $DIRECTORY"
